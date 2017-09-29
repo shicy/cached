3a003729e95875d2c41d7537b4784d1430267172
shicy <shicy85@163.com>
2017-09-29 17:48:34 +0800

    缓存客户端修改

--- cache_api/src/main/java/org/scy/cache/CachedClient.java
+++ cache_api/src/main/java/org/scy/cache/CachedClient.java
@@ -1,6 +1,6 @@
 package org.scy.cache;
 
-import org.scy.cache.model.CacheVO;
+import org.scy.common.web.controller.HttpResult;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +14,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public interface CachedClient {
 
     @RequestMapping(value = "get/{key}", method = RequestMethod.GET)
-    CacheVO get(@PathVariable("key") String key);
+    HttpResult get(@PathVariable("key") String key);
+
+
+    @RequestMapping(value = "set/{key}/{value}", method = RequestMethod.POST)
+    HttpResult set(@PathVariable("key") String key, @PathVariable("value") String value);
+
+    @RequestMapping(value = "set/{key}/{value}/{expires}", method = RequestMethod.POST)
+    HttpResult set(@PathVariable("key") String key, @PathVariable("value") String value,
+            @PathVariable("expires") int expires);
+
+    @RequestMapping(value = "set/{key}/{value}/{expires}/{flags}", method = RequestMethod.POST)
+    HttpResult set(@PathVariable("key") String key, @PathVariable("value") String value,
+            @PathVariable("expires") int expires, @PathVariable("flags") int flags);
+
+
+    @RequestMapping(value = "delete/{key}", method = RequestMethod.POST)
+    HttpResult delete(@PathVariable("key") String key);
 
 }
