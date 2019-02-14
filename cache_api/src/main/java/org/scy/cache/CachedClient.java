b40762cd59a9a28ecfebce0c8a84eb7fe45190ba
shicy <shicy85@163.com>
2019-02-14 17:38:53 +0800

    添加注释

--- cache_api/src/main/java/org/scy/cache/CachedClient.java
+++ cache_api/src/main/java/org/scy/cache/CachedClient.java
@@ -13,33 +13,100 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @FeignClient(name = "cache-service", url = "${app.cache-service.url:/}")
 public interface CachedClient {
 
+    /**
+     * 获取缓存对象
+     */
     @RequestMapping(value = "get/{key}", method = RequestMethod.GET)
     HttpResult get(@PathVariable("key") String key);
 
+    /**
+     * 获取缓存对象
+     * @param keys 逗号分隔
+     */
+    @RequestMapping(value = "gets/{keys}", method = RequestMethod.GET)
+    HttpResult gets(@PathVariable("keys") String keys);
 
+
+    /**
+     * 设置
+     * @return 设置成功返回 STORED，否则返回 ERROR
+     */
     @RequestMapping(value = "set/{key}/{value}", method = RequestMethod.POST)
     HttpResult set(@PathVariable("key") String key, @PathVariable("value") String value);
 
+    /**
+     * 设置
+     * @return 设置成功返回 STORED，否则返回 ERROR
+     */
     @RequestMapping(value = "set/{key}/{value}/{expires}", method = RequestMethod.POST)
     HttpResult set(@PathVariable("key") String key, @PathVariable("value") String value,
             @PathVariable("expires") int expires);
 
+    /**
+     * 设置
+     * @return 设置成功返回 STORED，否则返回 ERROR
+     */
     @RequestMapping(value = "set/{key}/{value}/{expires}/{flags}", method = RequestMethod.POST)
     HttpResult set(@PathVariable("key") String key, @PathVariable("value") String value,
             @PathVariable("expires") int expires, @PathVariable("flags") int flags);
 
 
+    /**
+     * 删除缓存对象
+     * @return 返回被删除的对象
+     */
     @RequestMapping(value = "delete/{key}", method = RequestMethod.POST)
     HttpResult delete(@PathVariable("key") String key);
 
+    /**
+     * 删除缓存对象
+     * @param keys 逗号分隔
+     * @return 返回被删除的对象
+     */
+    @RequestMapping(value = "deletes/{keys}", method = RequestMethod.POST)
+    HttpResult deletes(@PathVariable("keys") String keys);
 
+    /**
+     * 删除缓存对象（模糊匹配）
+     * @return 返回被删除的对象
+     */
+    @RequestMapping(value = "delete/like/{key}", method = RequestMethod.POST)
+    HttpResult deleteLike(@PathVariable("key") String key);
+
+    /**
+     * 删除缓存对象（以关键字开头的缓存）
+     * @return 返回被删除的对象
+     */
+    @RequestMapping(value = "delete/start/{key}", method = RequestMethod.POST)
+    HttpResult deleteStart(@PathVariable("key") String key);
+
+    /**
+     * 删除缓存对象（以关键字结尾的缓存）
+     * @return 返回被删除的对象
+     */
+    @RequestMapping(value = "delete/end/{key}", method = RequestMethod.POST)
+    HttpResult deleteEnd(@PathVariable("key") String key);
+
+
+    /**
+     * 更新
+     * @return 更新成功返回 STORED，否则返回 NOT_STORED
+     */
     @RequestMapping(value = "replace/{key}/{value}", method = RequestMethod.POST)
     HttpResult replace(@PathVariable("key") String key, @PathVariable("value") String value);
 
+    /**
+     * 更新
+     * @return 更新成功返回 STORED，否则返回 NOT_STORED
+     */
     @RequestMapping(value = "replace/{key}/{value}/{expires}", method = RequestMethod.POST)
     HttpResult replace(@PathVariable("key") String key, @PathVariable("value") String value,
             @PathVariable("expires") int expires);
 
+    /**
+     * 更新
+     * @return 更新成功返回 STORED，否则返回 NOT_STORED
+     */
     @RequestMapping(value = "replace/{key}/{value}/{expires}/{flags}", method = RequestMethod.POST)
     HttpResult replace(@PathVariable("key") String key, @PathVariable("value") String value,
             @PathVariable("expires") int expires, @PathVariable("flags") int flags);
--- cache_api/src/main/java/org/scy/cache/CachedClientAdapter.java
+++ cache_api/src/main/java/org/scy/cache/CachedClientAdapter.java
@@ -1,18 +1,20 @@
 package org.scy.cache;
 
 import org.scy.cache.model.CachedVO;
+import org.scy.common.utils.ArrayUtilsEx;
 import org.scy.common.web.controller.HttpResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 /**
  * 缓存客户端适配器
  * Created by shicy on 2017/9/30.
  */
 @Component
-@SuppressWarnings("unused")
+@SuppressWarnings({"unused", "unchecked"})
 public final class CachedClientAdapter {
 
     private static CachedClient cachedClient;
@@ -25,48 +27,156 @@ public final class CachedClientAdapter {
         cachedClient = cachedClientTemp;
     }
 
+    /**
+     * 获取缓存对象
+     */
     public static CachedVO get(String key) {
         HttpResult result = cachedClient.get(key);
         return result.getData(CachedVO.class);
     }
 
+    /**
+     * 获取多个缓存对象
+     * @param keys 键值，逗号分隔
+     */
+    public static CachedVO[] get(String[] keys) {
+        HttpResult result = cachedClient.gets(ArrayUtilsEx.join(keys, ","));
+        return getResultDatas(result);
+    }
+
+    /**
+     * 获取缓存内容
+     */
     public static String getValue(String key) {
-        CachedVO cachedVO = get(key);
+        HttpResult result = cachedClient.get(key);
+        CachedVO cachedVO = result.getData(CachedVO.class);
         return cachedVO != null ? cachedVO.getValue() : null;
     }
 
+    /**
+     * 设置
+     * @param key 关键字
+     * @param value 值
+     */
     public static boolean set(String key, String value) {
         HttpResult result = cachedClient.set(key, value);
         return CachedVO.STORED.equals(result.getData());
     }
 
+    /**
+     * 设置
+     * @param key 关键字
+     * @param value 值
+     * @param expires 有效期（秒）
+     */
     public static boolean set(String key, String value, int expires) {
         HttpResult result = cachedClient.set(key, value, expires);
         return CachedVO.STORED.equals(result.getData());
     }
 
+    /**
+     * 设置
+     * @param key 关键字
+     * @param value 值
+     * @param expires 有效期（秒）
+     * @param flags 缓存标记
+     */
     public static boolean set(String key, String value, int expires, int flags) {
         HttpResult result = cachedClient.set(key, value, expires, flags);
         return CachedVO.STORED.equals(result.getData());
     }
 
-    public static HttpResult delete(String key) {
-        return cachedClient.delete(key);
+    /**
+     * 删除缓存对象
+     * @return 返回被删除的对象
+     */
+    public static CachedVO delete(String key) {
+        HttpResult result = cachedClient.delete(key);
+        return (CachedVO)result.getData();
     }
 
+    /**
+     * 删除多个缓存对象
+     * @param keys 关键字逗号分隔
+     * @return 返回被删除的对象
+     */
+    public static CachedVO[] delete(String[] keys) {
+        HttpResult result = cachedClient.deletes(ArrayUtilsEx.join(keys, ","));
+        return getResultDatas(result);
+    }
+
+    /**
+     * 删除缓存对象，关键字模糊匹配
+     * @return 返回被删除的对象
+     */
+    public static CachedVO[] deleteLike(String key) {
+        HttpResult result = cachedClient.deleteLike(key);
+        return getResultDatas(result);
+    }
+
+    /**
+     * 删除某个关键字开头的缓存对象
+     * @return 返回被删除的对象
+     */
+    public static CachedVO[] deleteStart(String key) {
+        HttpResult result = cachedClient.deleteStart(key);
+        return getResultDatas(result);
+    }
+
+    /**
+     * 删除某个关键字结尾的缓存对象
+     * @return 返回被删除的对象
+     */
+    public static CachedVO[] deleteEnd(String key) {
+        HttpResult result = cachedClient.deleteEnd(key);
+        return getResultDatas(result);
+    }
+
+    /**
+     * 更新
+     * @param key 关键字
+     * @param value 值
+     */
     public static boolean replace(String key, String value) {
         HttpResult result = cachedClient.replace(key, value);
         return CachedVO.STORED.equals(result.getData());
     }
 
+    /**
+     * 更新
+     * @param key 关键字
+     * @param value 值
+     * @param expires 有效期（秒）
+     */
     public static boolean replace(String key, String value, int expires) {
         HttpResult result = cachedClient.replace(key, value, expires);
         return CachedVO.STORED.equals(result.getData());
     }
 
+    /**
+     * 更新
+     * @param key 关键字
+     * @param value 值
+     * @param expires 有效期（秒）
+     * @param flags 缓存标记
+     */
     public static boolean replace(String key, String value, int expires, int flags) {
         HttpResult result = cachedClient.replace(key, value, expires, flags);
         return CachedVO.STORED.equals(result.getData());
+    }
+
+    private static CachedVO[] getResultDatas(HttpResult result) {
+        Object resultData = result.getData();
+        if (resultData != null) {
+            try {
+                List<CachedVO> datas = (List<CachedVO>)resultData;
+                return datas.toArray(new CachedVO[0]);
+            }
+            catch (Exception e) {
+                //
+            }
+        }
+        return new CachedVO[0];
     }
 
 }
