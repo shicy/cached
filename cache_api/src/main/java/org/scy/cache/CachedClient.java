94315598b787663a320a55814a3142e13bc73d82
shicy <shicy85@163.com>
2019-11-05 18:25:45 +0800

    CachedClient修改

--- cache_api/src/main/java/org/scy/cache/CachedClient.java
+++ cache_api/src/main/java/org/scy/cache/CachedClient.java
@@ -16,14 +16,14 @@ public interface CachedClient {
     /**
      * 获取缓存对象
      */
-    @RequestMapping(value = "get/{key}", method = RequestMethod.GET)
+    @RequestMapping(value = "/get/{key}", method = RequestMethod.GET)
     HttpResult get(@PathVariable("key") String key);
 
     /**
      * 获取缓存对象
      * @param keys 逗号分隔
      */
-    @RequestMapping(value = "gets/{keys}", method = RequestMethod.GET)
+    @RequestMapping(value = "/gets/{keys}", method = RequestMethod.GET)
     HttpResult gets(@PathVariable("keys") String keys);
 
 
@@ -31,14 +31,14 @@ public interface CachedClient {
      * 设置
      * @return 设置成功返回 STORED，否则返回 ERROR
      */
-    @RequestMapping(value = "set/{key}/{value}", method = RequestMethod.POST)
+    @RequestMapping(value = "/set/{key}/{value}", method = RequestMethod.POST)
     HttpResult set(@PathVariable("key") String key, @PathVariable("value") String value);
 
     /**
      * 设置
      * @return 设置成功返回 STORED，否则返回 ERROR
      */
-    @RequestMapping(value = "set/{key}/{value}/{expires}", method = RequestMethod.POST)
+    @RequestMapping(value = "/set/{key}/{value}/{expires}", method = RequestMethod.POST)
     HttpResult set(@PathVariable("key") String key, @PathVariable("value") String value,
             @PathVariable("expires") int expires);
 
@@ -46,7 +46,7 @@ public interface CachedClient {
      * 设置
      * @return 设置成功返回 STORED，否则返回 ERROR
      */
-    @RequestMapping(value = "set/{key}/{value}/{expires}/{flags}", method = RequestMethod.POST)
+    @RequestMapping(value = "/set/{key}/{value}/{expires}/{flags}", method = RequestMethod.POST)
     HttpResult set(@PathVariable("key") String key, @PathVariable("value") String value,
             @PathVariable("expires") int expires, @PathVariable("flags") int flags);
 
@@ -55,7 +55,7 @@ public interface CachedClient {
      * 删除缓存对象
      * @return 返回被删除的对象
      */
-    @RequestMapping(value = "delete/{key}", method = RequestMethod.POST)
+    @RequestMapping(value = "/delete/{key}", method = RequestMethod.POST)
     HttpResult delete(@PathVariable("key") String key);
 
     /**
@@ -63,28 +63,28 @@ public interface CachedClient {
      * @param keys 逗号分隔
      * @return 返回被删除的对象
      */
-    @RequestMapping(value = "deletes/{keys}", method = RequestMethod.POST)
+    @RequestMapping(value = "/deletes/{keys}", method = RequestMethod.POST)
     HttpResult deletes(@PathVariable("keys") String keys);
 
     /**
      * 删除缓存对象（模糊匹配）
      * @return 返回被删除的对象
      */
-    @RequestMapping(value = "delete/like/{key}", method = RequestMethod.POST)
+    @RequestMapping(value = "/delete/like/{key}", method = RequestMethod.POST)
     HttpResult deleteLike(@PathVariable("key") String key);
 
     /**
      * 删除缓存对象（以关键字开头的缓存）
      * @return 返回被删除的对象
      */
-    @RequestMapping(value = "delete/start/{key}", method = RequestMethod.POST)
+    @RequestMapping(value = "/delete/start/{key}", method = RequestMethod.POST)
     HttpResult deleteStart(@PathVariable("key") String key);
 
     /**
      * 删除缓存对象（以关键字结尾的缓存）
      * @return 返回被删除的对象
      */
-    @RequestMapping(value = "delete/end/{key}", method = RequestMethod.POST)
+    @RequestMapping(value = "/delete/end/{key}", method = RequestMethod.POST)
     HttpResult deleteEnd(@PathVariable("key") String key);
 
 
@@ -92,14 +92,14 @@ public interface CachedClient {
      * 更新
      * @return 更新成功返回 STORED，否则返回 NOT_STORED
      */
-    @RequestMapping(value = "replace/{key}/{value}", method = RequestMethod.POST)
+    @RequestMapping(value = "/replace/{key}/{value}", method = RequestMethod.POST)
     HttpResult replace(@PathVariable("key") String key, @PathVariable("value") String value);
 
     /**
      * 更新
      * @return 更新成功返回 STORED，否则返回 NOT_STORED
      */
-    @RequestMapping(value = "replace/{key}/{value}/{expires}", method = RequestMethod.POST)
+    @RequestMapping(value = "/replace/{key}/{value}/{expires}", method = RequestMethod.POST)
     HttpResult replace(@PathVariable("key") String key, @PathVariable("value") String value,
             @PathVariable("expires") int expires);
 
@@ -107,7 +107,7 @@ public interface CachedClient {
      * 更新
      * @return 更新成功返回 STORED，否则返回 NOT_STORED
      */
-    @RequestMapping(value = "replace/{key}/{value}/{expires}/{flags}", method = RequestMethod.POST)
+    @RequestMapping(value = "/replace/{key}/{value}/{expires}/{flags}", method = RequestMethod.POST)
     HttpResult replace(@PathVariable("key") String key, @PathVariable("value") String value,
             @PathVariable("expires") int expires, @PathVariable("flags") int flags);
 
