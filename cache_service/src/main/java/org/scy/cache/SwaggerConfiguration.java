f9fb8e787adb940bd97bb2f67df4a4acdd1e11fc
shicy <shicy85@163.com>
2020-07-29 17:45:11 +0800

    添加 swagger 配置

---
+++ cache_service/src/main/java/org/scy/cache/SwaggerConfiguration.java
@@ -0,0 +1,31 @@
+package org.scy.cache;
+
+import org.scy.common.configs.BaseSwaggerConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+@Profile({"dev"})
+public class SwaggerConfiguration extends BaseSwaggerConfiguration {
+    @Override
+    protected String getTitle() {
+        return "Cached Service API";
+    }
+
+    @Override
+    protected String getDescription() {
+        return "缓存服务接口";
+    }
+
+    @Override
+    protected String getServiceUrl() {
+        return "http://cached.scyok.com";
+    }
+
+    @Override
+    protected String getApiPackage() {
+        return "org.scy.cache.controller";
+    }
+}
